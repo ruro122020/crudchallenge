c252767e89eecb1704665ffe9dfe4e8b8c1b936e
Ruth <ruro122020@gmail.com>
2023-12-11 17:07:41 -0500

    save and create method complete

---
+++ book.py
@@ -0,0 +1,54 @@
+from __init__ import CONN, CURSOR
+
+class Book:
+
+    all = {}
+
+    def __init__(self, title, author, published_year, library_id):
+        self.id = None
+        self.title = title
+        self.author = author
+        self.published_year = published_year
+        self.library_id = library_id
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            author TEXT,
+            published_year INTEGER,
+            library_id INTEGER,
+            FOREIGN KEY (library_id) REFERENCES libraries(id))
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS books
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def create(cls, title, author, published_year, library_id):
+        book = cls(title, author, published_year, library_id)
+        book.save()
+        return book
+    
+    def save(self):
+        sql ="""
+            INSERT INTO books (title, author, published_year, library_id)
+            VALUES (?, ?, ?, ?)
+        """
+
+        CURSOR.execute(sql, (self.title, self.author, self.published_year, self.library_id))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
--- debug.py
+++ debug.py
@@ -1,18 +1,24 @@
 from __init__ import CONN, CURSOR  
 import ipdb
 from library import Library
-
+from book import Book
 
 Library.drop_table()
 Library.create_table()
+
+Book.drop_table()
+Book.create_table()
 
 nooks = Library.create('Nooks')
 fisk = Library.create('Fisk')
 google = Library.create('Google')
 
+Book.create('Remember Me', 'Christopher Pike', 1980, nooks.id)
+Book.create('IT', 'Stephen King', 1970, nooks.id)
+Book.create("World War II", "Eve Sanders", 2000, fisk.id)
 
-
-CURSOR.execute("""SELECT * FROM libraries""")
-libraries = CURSOR.fetchall()
+libraries = CURSOR.execute("""SELECT * FROM libraries""").fetchall()
+books = CURSOR.execute("""SELECT * FROM  books""").fetchall()
 print(libraries)
+print(books)
 # ipdb.set_trace()
